f3e9f62e3550b589b5a593193b0fc2f72f860d6b
richardmundyiii <mundy.richard@icloud.com>
2023-12-22 23:14:03 -0800

    create team

---
+++ src/components/ShowAllTeams/ShowAllTeams.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import * as TeamsApi from "../../utilities/teams-api";
+
+export default function ShowAllTeams() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    async function getAllTeams() {
+      const teams = await TeamsApi.getAllTeams();
+      setTeams(teams);
+    }
+    getAllTeams();
+  }, []);
+
+  return (
+    <>
+      <h1>Show All Teams</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Venue</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map((team, idx) => (
+            <tr key={idx}>
+              <td>{team.name}</td>
+              <td>{team.venue}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
